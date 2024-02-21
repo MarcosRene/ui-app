1acc96255c7ae761dcbdc9705a47e9a8af5e775f
Marcos Renê <renemoura789@gmail.com>
2024-02-21 11:23:28 -0300

    feat: add getSlugFromString util

---
+++ src/utils/getSlugFromString.tsx
@@ -0,0 +1,8 @@
+export function getSlugFromString(value: string): string {
+  return value
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .replace(/[^\w\s]/g, '')
+    .replace(/\s+/g, '-')
+}
